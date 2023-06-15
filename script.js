c2bcfe2f053a66d73c798d9f2c0b129a57c74c49
ebuggs <ebuggs212@gmail.com>
2023-06-15 12:36:27 -0400

    add for each function to display each button number on current

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+let current = document.querySelector(".current");
+let number = document.querySelectorAll(".number");
+
+number.forEach(function (number) {
+  number.addEventListener("click", function () {
+    current.textContent += number.textContent;
+  });
+});
