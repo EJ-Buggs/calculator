6886db0c45bf14d7a1c10a2d605d8ee063706f18
ebuggs <ebuggs212@gmail.com>
2023-06-19 06:27:08 -0400

    add dot and delete function, toggle for operator switch, totaling continous operations, resets for numbers after equaling and improved css

--- script.js
+++ script.js
@@ -3,14 +3,23 @@ let current = document.querySelector(".current");
 let previous = document.querySelector(".previous");
 let number = document.querySelectorAll(".number");
 let operators = document.querySelectorAll(".operator");
+let del = document.querySelector(".delete");
 let op = "";
+let op2 = "";
 let c = "";
 let p = "";
 let thing = true;
 
+const backspace = function () {
+  current.textContent = current.textContent.slice(0, -1);
+};
+
+del.addEventListener("click", backspace);
+
 const clear = function () {
   c = 0;
   p = 0;
+  thing = true;
   current.textContent = "0";
   previous.textContent = "";
 };
@@ -25,11 +34,26 @@ number.forEach(function (number) {
 });
 
 const operate = function () {
-  console.log(p, c);
   op = this.textContent;
-  thing == true ? (c = Number(current.textContent)) : (thing = true);
-  previous.textContent += current.textContent + op;
-  p == 0 ? (p = Number(current.textContent)) : calculate();
+  previous.textContent == "" ? (op2 = op) : null;
+  if (op2 !== op) {
+    c = Number(current.textContent);
+    calculate();
+    previous.textContent = `${p}${op}`;
+    current.textContent = "";
+    op2 = op;
+    return;
+  }
+  if (thing == true) {
+    c = Number(current.textContent);
+    p == 0 ? (p = Number(current.textContent)) : calculate();
+  } else {
+    previous.textContent = current.textContent + `${op}`;
+    current.textContent = "";
+    thing = true;
+    return;
+  }
+  previous.textContent = `${p} ${op}`;
   current.textContent = "";
 };
 
@@ -38,13 +62,13 @@ operators.forEach(function (operators) {
 });
 
 const calculate = function () {
-  if (op == "+") {
+  if (op2 == "+") {
     p += c;
-  } else if (op == "-") {
+  } else if (op2 == "-") {
     p -= c;
-  } else if (op == "x") {
+  } else if (op2 == "x") {
     p *= c;
-  } else if (op == "/") {
+  } else if (op2 == "/") {
     p /= c;
   }
   current.textContent = p;
@@ -53,12 +77,10 @@ const calculate = function () {
 const equals = function () {
   if (thing == true) {
     c = Number(current.textContent);
-    previous.textContent += current.textContent + " =";
+    thing = false;
   }
-  thing = false;
+  previous.textContent = `${p} ${op} ${c} =`;
   calculate();
-  previous.textContent = `${p} + ${c} =`;
-  console.log(p, c);
 };
 
 document.querySelector(".equals").addEventListener("click", equals);
